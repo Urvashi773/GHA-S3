8ca65b84fda4e8e69f7b2b41c0562feba8836625
Urvashi773 <136550193+Urvashi773@users.noreply.github.com>
2026-01-13 17:09:39 +0530

    Delete dist directory

--- dist/app.js
+++
@@ -1,6 +0,0 @@
-(function () {
-  const el = document.getElementById('status');
-  if (!el) return;
-  const now = new Date();
-  el.textContent = `Client JS loaded at: ${now.toLocaleString()}`;
-})();
